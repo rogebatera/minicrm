b22b30d56d02e33db51469efb28bd46f29f8289a
Rogerio Lucio <rogeriob.lucio@hotmail.com>
2026-04-13 07:49:10 -0300

    ajuste tela de customer list

---
+++ src/app/paginas/customerpage/customerpage-old.ts
@@ -0,0 +1,66 @@
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
+import { PoInfoModule, PoListViewModule, PoLoadingModule, PoModalComponent, PoModalModule, PoPageFilter, PoPageModule } from '@po-ui/ng-components';
+import { Customer } from '../../classes/customer';
+import { CustomerService } from '../../services/customer'; 
+
+@Component({
+  selector: 'app-customerpage',
+  imports: [PoListViewModule, PoPageModule, PoInfoModule, PoModalModule, PoLoadingModule],
+  templateUrl: './customerpage.html',
+  styleUrl: './customerpage.css',
+})
+export class Customerpage implements OnInit{
+
+  isLoading: boolean = true
+  customerList: Array<Customer> = []
+  customerListFiltered: Array<Customer> = []
+  #customerService = inject(CustomerService)
+  detailCustomer: Customer = new Customer()
+  filterSettings: PoPageFilter = { placeholder: 'Filtrar por nome ou endereco', action: this.customerFilter.bind(this)}
+
+  @ViewChild('modalCustomer') modalCustomerElement!: PoModalComponent;
+
+  ngOnInit():void {
+    this.loadData()
+  }
+
+  loadData():void{
+    let req = this.#customerService.getCustomers()
+    let itens: Array<Customer> = []
+    const hideload = () => this.isLoading = false;
+
+    req.subscribe({
+      next(value: any){
+        value.items.forEach((el: Customer) => itens.push(el));
+        hideload()
+      },
+      error(error){
+        console.log(`error req customer list`, error)
+      },
+      complete() {
+        console.log(`complete req customer list`)
+      }
+    })
+
+    this.customerList = itens
+    this.customerListFiltered = itens
+
+    // setTimeout(() => {
+    //   this.isLoading = false 
+    // }, 1000);
+
+  }
+
+  showDetail(customer: Customer): void {
+    this.detailCustomer = customer;
+    this.modalCustomerElement.open();
+  }
+
+  customerFilter(content: string){
+    this.customerListFiltered = this.customerList.filter(customer => 
+    customer.nome.indexOf(content.toUpperCase()) >= 0 || 
+    customer.endereco.indexOf(content.toUpperCase()) >= 0)
+    console.log(`testessss`)
+  }
+
+}
--- src/app/paginas/customerpage/customerpage.ts
+++ src/app/paginas/customerpage/customerpage.ts
@@ -1,62 +1,65 @@
-import { Component, inject, OnInit, ViewChild } from '@angular/core';
-import { PoInfoModule, PoListViewModule, PoLoadingModule, PoModalComponent, PoModalModule, PoPageFilter, PoPageModule } from '@po-ui/ng-components';
+import { Component, inject, OnInit, ViewChild} from '@angular/core';
+import { PoBreadcrumbModule, PoInfoModule, PoListViewModule, PoLoadingModule, PoModalComponent, PoModalModule, PoPageFilter, PoPageModule, PoPageSlideModule } from '@po-ui/ng-components';
 import { Customer } from '../../classes/customer';
 import { CustomerService } from '../../services/customer';
+//import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-customerpage',
-  imports: [PoListViewModule, PoPageModule, PoInfoModule, PoModalModule, PoLoadingModule],
+  standalone: true,
+  imports: [PoPageModule,PoListViewModule,PoInfoModule,PoModalModule,PoLoadingModule,PoBreadcrumbModule,PoPageSlideModule],
   templateUrl: './customerpage.html',
-  styleUrl: './customerpage.css',
+  styleUrl: './customerpage.css'
 })
 export class Customerpage implements OnInit{
 
-  customerList: Array<Customer> = []
-  customerListFiltered: Array<Customer> = []
-  #customerService = inject(CustomerService)
-  detailCustomer: Customer = new Customer()
-  filterSettings: PoPageFilter = { placeholder: 'Filtrar por nome ou endereco', action: this.customerFilter.bind(this)}
+    isLoading: boolean = true
+    customerList: Array<Customer> = []
+    customerListFiltered: Array<Customer> = []
+    detailCustomer: Customer = new Customer()
+    #customerService = inject(CustomerService)
+    filterSettings: PoPageFilter = {placeholder: "Filtrar por nome ou endereco", action: this.customerFilter.bind(this)}
 
-  @ViewChild('modalCustomer') modalCustomerElement!: PoModalComponent
+    @ViewChild('modalCustomer') modalCustomerElement!: PoModalComponent;
 
-  ngOnInit():void {
-    this.loadData()
-  }
+    ngOnInit(): void {
+      this.loadData()
+    }
 
-  loadData():void{
-    let req = this.#customerService.getCustomers()
-    let itens: Array<Customer> = []
+    loadData():void {
+      
+      let req = this.#customerService.getCustomers()
+      let itens: Array<Customer> = []
+      const hideload = () => this.isLoading = false;
 
-    req.subscribe({
-      next(value: any){
-        value.items.forEach((el: Customer) => {
-          itens.push(el)
-        });
-      },
-      error(error){
-        console.log(`error req customer list`, error)
-      },
-      complete() {
-        //console.log(`complete req customer list`)
-      }
-    })
+      req.subscribe({
+        next(value: any) {
+          value.items.forEach((el: Customer) => itens.push(el));
+          hideload()
+        },
+        error(err) {
+          //console.log(`error req customer`,err)
+        },
+        complete() {
+          //console.log(`complete customer list`)
+        },
+      })
 
-    this.customerList = itens
-    this.customerListFiltered = itens
+      this.customerList = itens
+      this.customerListFiltered = itens
+      // this.#customerService.setListCustomer(itens)
 
-    console.log(`customer list: `, this.customerListFiltered)
-  }
+    }
 
-  showDetail(customer: Customer): void {
-    this.detailCustomer = customer;
-    this.modalCustomerElement.open();
-  }
+    showDetail(customer: Customer):void {
+      this.detailCustomer = customer;
+      this.modalCustomerElement.open();
+    }
 
-  customerFilter(content: string){
-    this.customerListFiltered = this.customerList.filter(customer => 
-    customer.nome.indexOf(content.toUpperCase()) >= 0 || 
-    customer.endereco.indexOf(content.toUpperCase()) >= 0)
-    console.log(`testessss`)
-  }
-
+    customerFilter(content:string):void {
+      console.log(`teste de filtro`)
+      this.customerListFiltered = this.customerList.filter(customer => 
+        customer.nome.indexOf(content.toUpperCase()) >= 0 || 
+        customer.endereco.indexOf(content.toUpperCase()) >= 0)
+    }
 }
